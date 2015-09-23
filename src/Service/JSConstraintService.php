65b9ee2dbcc624732a3568ef6c48e6806f4bee7f
Joseph Brewer <joseph_brewer@apple.com>
2015-09-23 11:19:37 -0500

    Added some base files

---
+++ src/Service/JSConstraintService.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace JJB\FormUtils\Service;
+
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints;
+use CSID\IMC\Baseline\CoreBundle\Component\RegexPatterns AS Pattern;
+use CSID\IMC\Baseline\PortalBundle\Validator\Constraints AS CustomConstraints;
+
+class JSConstraintService
+{
+    /**
+     * Types of constraints that we currently support
+     */
+    const CONSTRAINT_TYPE_LENGTH            = 'Length';
+    const CONSTRAINT_TYPE_REGEX             = 'Regex';
+    const CONSTRAINT_TYPE_NOTBLANK          = 'NotBlank';
+    const CONSTRAINT_TYPE_EMAIL             = 'Email';
+    const CUSTOM_CONSTRAINT_TYPE_MATCH      = 'Match';
+    const CUSTOM_CONSTRAINT_TYPE_GROUP      = 'Group';
+    const CUSTOM_CONSTRAINT_TYPE_LUHN       = 'Luhn';
+    const CUSTOM_CONSTRAINT_TYPE_BINCODE    = 'BinCodeConstraint';
+
+    /**
+     * @var Translator
+     */
+    private $translator;
+
+
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param Constraint $c
+     * @param null|string $domain
+     * @param null|array $tests
+     * @param null|array $messages
+     * @return array
+     */
+    public function extractConstraints(Constraint $c, $domain = null, $tests = [], $messages = [])
+    {
+        $namespace  = get_class($c);
+        $parts      = explode('\\', $namespace);
+        $class      = array_pop($parts);
+
+        switch ($class) {
+
+            case self::CONSTRAINT_TYPE_NOTBLANK:
+                /** @var Constraints\NotBlank $c */
+                $tests[]    = Pattern::NOT_BLANK;
+                $messages[] = $this->trans($c->message, [], $domain);
+
+                break;
+
+            case self::CONSTRAINT_TYPE_LENGTH:
+                /** @var Constraints\Length $c */
+                if ($c->min > 0 && $c->max > 0 && ($c->min === $c->max)) {
+                    $tests[]    = '^.{' . $c->min . ',' . $c->max . '}$';
+                    $messages[] = str_replace(
+                        '{{ '.$c->min.' }}',
+                        $c->min, // can use either min or max (they're the same)
+                        $this->trans($c->exactMessage, [
+                            'limit' => $c->min
+                        ], $domain)
+                    );
+                } else {
+                    if ($c->min > 0) {
+                        $tests[]    = '.{' . $c->min . ',}';
+                        $messages[] = str_replace(
+                            '{{ '.$c->min.' }}',
+                            $c->min,
+                            $this->trans($c->minMessage, [
+                                'limit' => $c->min
+                            ], $domain)
+                        );
+                    }
+
+                    if ($c->max > 0) {
+                        $tests[]    = '^.{0,' . $c->max . '}$';
+                        $messages[] = str_replace(
+                            '{{ '.$c->max.' }}',
+                            $c->max,
+                            $this->trans($c->maxMessage, [
+                                'limit' => $c->max
+                            ], $domain)
+                        );
+                    }
+                }
+
+                break;
+
+            case self::CONSTRAINT_TYPE_REGEX:
+                /** @var Constraints\Regex $c */
+                $tests[]    = str_replace('/', '', $c->pattern);
+                $messages[] = $this->trans($c->message, [], $domain);
+
+                break;
+
+            case self::CUSTOM_CONSTRAINT_TYPE_MATCH:
+                /** @var CustomConstraints\Match $c */
+                $tests[]    = '__MATCH__' . ($c->field_id ? $c->field_id : $c->field);
+                $messages[] = $this->trans($c->message, [], $domain);
+
+                break;
+
+            case self::CUSTOM_CONSTRAINT_TYPE_GROUP:
+                /** @var CustomConstraints\Group $c */
+                $tests[]    = '__GROUP__' . implode(array_keys($c->fields), ',');
+                $messages[] = $this->trans($c->message, [], $domain);
+
+                break;
+
+            case self::CUSTOM_CONSTRAINT_TYPE_LUHN:
+                /** @var CustomConstraints\Luhn $c */
+                $tests[]    = '__LUHN__' . $c->field_id;
+                $messages[] = $this->trans($c->message, [], $domain);
+                break;
+
+            case self::CUSTOM_CONSTRAINT_TYPE_BINCODE:
+                /** @var CustomConstraints\BincodeConstraint $c */
+                $tests[]    = '__BINCODE__';
+                $messages[] = $this->trans($c->message, [], $domain);
+                break;
+
+            case self::CONSTRAINT_TYPE_EMAIL:
+                /** @var Constraints\Email $c */
+                $tests[]    = Pattern::EMAIL;
+                $messages[] = $this->trans($c->message, [], $domain);
+                break;
+
+            default:
+                break;
+        }
+
+        return [$tests, $messages];
+    }
+
+    /**
+     * @param string $string
+     * @param string array $params
+     * @param $domain
+     * @return string
+     */
+    private function trans($string, array $params = [], $domain)
+    {
+        return $this->translator->trans($string, $params, $domain);
+    }
+}
---
+++ src/Twig/JSConstraintExtension.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace JJB\Formutils\Twig;
+
+use CSID\IMC\Baseline\CoreBundle\Service\JSConstraintService;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints;
+
+class JSConstraintExtension extends \Twig_Extension
+{
+    /**
+     * @var JSConstraintService
+     */
+    private $jsConstraintService;
+
+
+    public function __construct(JSConstraintService $jsConstraintService)
+    {
+        $this->jsConstraintService = $jsConstraintService;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('add_js_constraints', [$this, 'addJsConstraints'])
+        ];
+    }
+
+    /**
+     * @param string $fieldName
+     * @param array $attributes
+     * @param Constraint[] $constraints
+     * @param null|string $domain
+     * @return string
+     * @throws \Exception
+     */
+    public function addJsConstraints($fieldName, array $attributes, array $constraints = [], $domain = null)
+    {
+        $tests    = [];
+        $messages = [];
+
+        if (!empty($constraints)) {
+            foreach ($constraints as $constraint) {
+                if (!$constraint instanceof Constraint) {
+                    throw new \Exception("The constraint provided for '{$fieldName}' is not an instance of Constraint. Please make sure you did not nest an array");
+                }
+
+                list ($tests, $messages) = $this->jsConstraintService->extractConstraints($constraint, $domain, $tests, $messages);
+
+            }
+
+            if (!empty($tests) && !empty($messages)) {
+                $attributes = array_merge($attributes, [
+                        'data-constraints'    => json_encode($tests),
+                        'data-error-messages' => json_encode($messages)
+                    ]
+                );
+            }
+        }
+
+        return $attributes;
+    }
+
+    public function getName()
+    {
+        return 'js_constraint_extension';
+    }
+}
+
